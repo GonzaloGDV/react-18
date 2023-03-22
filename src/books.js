e428194d4a8b72cacbbdbad7909bfcbfe94a7fd8
Gonzalo Gomez <gonzalogdv@gmail.com>
2023-03-22 11:18:17 -0300

    Components

---
+++ src/books.js
@@ -0,0 +1,27 @@
+import img1 from './images/book1.jpg';
+import img2 from './images/book2.jpg';
+import img3 from './images/book3.jpeg';
+
+const books = [
+  {
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    id: 1,
+    img: img1,
+  },
+
+  {
+    title: 'Interesting Facts For Curious Minds',
+    author: 'Jordan Moore',
+    id: 2,
+    img: img2,
+  },
+  {
+    title: 'The creative Act. A way of Being',
+    author: 'Rick Rubin',
+    id: 3,
+    img: img3,
+  },
+];
+
+export default books;
--- src/index.js
+++ src/index.js
@@ -1,45 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-const firstBook = {
-  title: 'Atomic Habits',
-  author: 'James Clear',
-  img: 'https://images-na.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
-};
-
-const secondBook = {
-  title: 'Interesting Facts For Curious Minds',
-  author: 'Jordan Moore',
-  img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/518z5dvykPL._SX348_BO1,204,203,200_.jpg',
-};
+import books from './books';
+import Book from './Book';
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        img={firstBook.img}
-      />
-      <Book
-        title={secondBook.title}
-        author={secondBook.author}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 }
-
-const Book = (props) => {
-  return (
-    <article className='book'>
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
-    </article>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
